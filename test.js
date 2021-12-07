4cdf5b060fe33d3363316bcc2eb01d0aa6b62f46
JonhyGuy <joulura1609@gmail.com>
2021-12-06 19:14:37 -0600

    new test

--- test.js
+++ test.js
@@ -92,7 +92,31 @@ describe('Prueba de Login', function (){
         await driver.quit();
         
     }
-    })   
+    })
+    it('Prueba ', async function(){
+      let driver = await new Builder().forBrowser('chrome').build();
+      let buscar = 'Juego';
+      try {
+        await driver.get('http://localhost:4200');
+        await driver.findElement(By.id('user')).sendKeys('god@gmail.com', Key.RETURN);
+        await driver.findElement(By.id('pass')).sendKeys('12345678', Key.RETURN);
+        await driver.findElement(By.id('commit')).click()
+        await sleep(13000);
+        await driver.findElement(By.id('show')).click()
+        await sleep(5000);
+        //url.substring()
+        const caso = await driver.wait(until.urlIs('http://localhost:4200/info/6'), 25000);
+        //const url = await driver.wait(until.getCurrentUrl(),10000)
+       // const url =  await driver.getCurrentUrl();
+      
+       
+          await assert.equal(caso,true);
+      } finally {
+        await sleep(1000);
+        await driver.quit();
+        
+    }
+    })    
   
 })
 
